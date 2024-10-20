c437ed21e69283271b8f0ad826ac246f8b1d49c1
Om Pawaskar <138382841+Ompawaskar@users.noreply.github.com>
2024-10-21 01:03:40 +0530

    Update Test.java 2

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        int number = 5; // Example input
+        int number = 4; // Example input
         int fact = 1;
         for (int i = 1; i <= number; i++) {
             fact *= i;
